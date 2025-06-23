6ae8c24e5ac565ed81f09bac25ee45e0fc1bae3b
NicolyMattos <nicoly.souza4@fatec.sp.gov.br>
2025-06-23 00:48:22 -0300

    ajustes para as camadas api, application  e infradata

--- InnatAPP/InnatAPP.Application/DTOs/Produto/ProdutoCreateDTO.cs
+++ InnatAPP/InnatAPP.Application/DTOs/Produto/ProdutoCreateDTO.cs
@@ -1,20 +1,15 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace InnatAPP.Application.DTOs
+namespace InnatAPP.Application.DTOs.Produto
 {
-    public class ProdutoDTO
+    public class ProdutoCreateDTO
     {
-        [Required(ErrorMessage = "O id é obrigatório.")]
-        [DisplayName("Id")]
-        public Guid Id { get; set; }
-
         [Required(ErrorMessage = "O nome é obrigatório.")]
         [MinLength(2, ErrorMessage = "O nome deve ter no mínimo 2 caracteres.")]
         [MaxLength(100, ErrorMessage = "O nome pode ter no máximo 100 caracteres.")]
         [DisplayName("Nome")]
         public string Nome { get; set; } = string.Empty;
-
 
         [Required(ErrorMessage = "A descrição é obrigatória.")]
         [MinLength(10, ErrorMessage = "A descrição deve ter no mínimo 10 caracteres.")]
@@ -26,13 +21,6 @@ namespace InnatAPP.Application.DTOs
         [DisplayName("Foto")]
         public string Foto { get; set; } = string.Empty;
 
-        [Range(0, 5, ErrorMessage = "A nota tem ser entre 0 e 5.")]
-        [DisplayName("Nota")]
-        public decimal Nota { get; set; }
-
-        [DisplayName("Total de Reviews")]
-        public int TotalReviews { get; set; }
-
         [Required(ErrorMessage = "O id de categoria é obrigatório.")]
         [DisplayName("Id Categoria")]
         public Guid IdCategoria { get; set; }
@@ -41,12 +29,5 @@ namespace InnatAPP.Application.DTOs
         [DisplayName("Id Empresa")]
         public Guid IdEmpresa { get; set; }
 
-        [Required(ErrorMessage = "Os dados da categoria são obrigatórios.")]
-        [DisplayName("Categoria")]
-        public CategoriaDTO Categoria { get; set; } = new CategoriaDTO();
-
-        [Required(ErrorMessage = "Os dados da empresa são obrigatórios.")]
-        [DisplayName("Empresa")]
-        public EmpresaDTO Empresa { get; set; } = new EmpresaDTO();
     }
 }
--- InnatAPP/InnatAPP.Domain/Entities/Avaliador.cs
+++ InnatAPP/InnatAPP.Domain/Entities/Avaliador.cs
@@ -43,7 +43,7 @@ namespace InnatAPP.Domain.Entities
 
             #region Inicialização das Propriedades
 
-            Id = Guid.NewGuid();
+            Id = idUsuario;
             IdUsuario = idUsuario;
 
             #endregion
